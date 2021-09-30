e6c6751ba2027bf307984fe7854c5055bd866e07
Gianluca <gianluca.ansaldi.1989@gmail.com>
2021-10-01 00:30:56 +0100

    added API to show current temperature in different cities

--- original/thermostat/interface.js
+++ original/thermostat/interface.js
@@ -4,6 +4,29 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector('#temperature').className = thermostat.energyUsage();
   }
 
+  const selectElement = document.querySelector('#current-city');
+  selectElement.addEventListener('change', (event) => {
+    const city = event.target.value;
+    
+    displayWeather(city);
+  });
+
+  const displayWeather = (city) => {
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric`
+  
+    fetch(url)
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        document.querySelector('#current-temperature').innerText = data.main.temp;
+      })
+  }
+
+  displayWeather('London');
+
+
+
   const thermostat = new Thermostat();
   updateTemperature();
 
@@ -26,11 +49,13 @@ document.addEventListener("DOMContentLoaded", () => {
     thermostat.switchPowerSavingModeOn();
     document.querySelector('#power-saving-status').innerText = 'on';
     updateTemperature();
-  })
+  });
 
   document.querySelector('#powersaving-off').addEventListener('click', () => {
     thermostat.switchPowerSavingModeOff();
     document.querySelector('#power-saving-status').innerText = 'off';
     updateTemperature();
-  })
+  });
+
+
 });
